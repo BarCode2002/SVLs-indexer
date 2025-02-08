f3478fedc8a5149f2a87885f46b6233ae35927c3
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-02-08 17:03:44 +0100

    avances

--- models/__init__.py
+++ models/__init__.py
@@ -2,14 +2,27 @@ from dipdup import fields
 from dipdup.models import Model
 
 class Holder(Model):
-    id = fields.CharField(max_length=100, primary_key=True)
-    address = fields.TextField()
+    svl_key = fields.CharField(max_length=100, primary_key=True)
+    
     vin = fields.TextField()
     brand = fields.TextField()
     model = fields.TextField()
     year = fields.TextField()
-    request = fields.TextField()
-    accept_request = fields.BooleanField()
-    prev_owners_info = fields.JSONField()
-    curr_owner_info = fields.JSONField()
-    price = fields.CharField(max_length=100)
+
+    vehicle_type = fields.TextField()
+    kilometers = fields.TextField()
+    shift = fields.TextField()
+    fuel = fields.TextField()
+    power = fields.TextField()
+    color = fields.TextField()
+    state = fields.TextField()
+    autonomy = fields.TextField()
+    climate = fields.TextField()
+    usage = fields.TextField()
+    storage = fields.TextField()
+
+    requester_address = fields.TextField()
+    request_accepted = fields.BooleanField()
+    previous_owners_info = fields.JSONField()
+    current_owner_info = fields.JSONField()
+    svl_price = fields.CharField(max_length=100)
