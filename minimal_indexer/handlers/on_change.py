fcd2b229996408aa7746a1ece2886f22729010bc
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-02-17 15:37:29 +0100

    avances

--- minimal_indexer/handlers/on_change.py
+++ minimal_indexer/handlers/on_change.py
@@ -10,13 +10,13 @@ async def on_change(
     svls: TezosBigMapDiff[SvlsKey, SvlsValue],
 ) -> None:
         if not svls.key: return
-        svl_key = svls.key.root
-        owner_address = svls.value.owner
-        vin = svls.value.VIN  
-        requester_address = svls.value.request                                                                    
-        request_accepted = svls.value.acceptRequest
-        curr_owner_info = svls.value.curr_owner_info
-        prev_owners_info = svls.value.prev_owners_info
+        svl_key=svls.key.root
+        owner_address=svls.value.owner
+        vin=svls.value.VIN  
+        requester_address=svls.value.request                                                                    
+        request_accepted=svls.value.acceptRequest
+        curr_owner_info=svls.value.curr_owner_info
+        prev_owners_info=svls.value.prev_owners_info
         p_o_i = []
         for o in prev_owners_info:
             #ctx.logger.info(o) http://host.docker.internal:8080/ipfs/
@@ -25,6 +25,9 @@ async def on_change(
 
         holder = await models.Holder.get_or_none(svl_key=svl_key)
         ctx.logger.info(curr_owner_info[0])
+        brand = 'empty'
+        model = 'empty'
+        year = 'empty'
         if (curr_owner_info[0] != ''):
             local_ipfs = ctx.get_http_datasource('local_ipfs')
             response = await local_ipfs.request(
@@ -34,59 +37,35 @@ async def on_change(
             ctx.logger.info(response[0]['brand'])
             ctx.logger.info(response[0]['year'])
             ctx.logger.info(response[0]['kilometers'])
-            brand = response[0]['brand']
-            year = response[0]['model']
-            model = response[0]['year']
-            if holder is None:
-                await models.Holder.create(
-                    svl_key=svl_key, 
-                    owner_address=owner_address,
-                    vin=vin,
-                    brand=brand,
-                    model=model,
-                    year=year,
-                    requester_address=requester_address,
-                    request_accepted=request_accepted,
-                    current_owner_info=curr_owner_info,
-                    previous_owners_info=p_o_i,
-                    svl_price=svl_price,
-                )
-            else:
-                holder.owner_address = owner_address
-                holder.vin = vin,
-                holder.brand=brand,
-                holder.model=model,
-                holder.year=year,      
-                holder.requester_address = requester_address
-                holder.request_accepted = request_accepted
-                holder.current_owner_info = curr_owner_info
-                holder.previous_owners_info = p_o_i
-                holder.svl_price = svl_price
-                await holder.save()
+            brand=response[0]['brand']
+            year=response[0]['model']
+            model=response[0]['year']        
+        if holder is None:
+            await models.Holder.create(
+                svl_key=svl_key, 
+                owner_address=owner_address,
+                vin=vin,
+                brand=brand,
+                model=model,
+                year=year,
+                requester_address=requester_address,
+                request_accepted=request_accepted,
+                current_owner_info=curr_owner_info,
+                previous_owners_info=p_o_i,
+                svl_price=svl_price,
+            )
         else:
-            if holder is None:
-                await models.Holder.create(
-                    svl_key=svl_key, 
-                    owner_address=owner_address,
-                    vin=vin,
-                    brand='',
-                    model='',
-                    year='',
-                    requester_address=requester_address,
-                    request_accepted=request_accepted,
-                    current_owner_info=curr_owner_info,
-                    previous_owners_info=p_o_i,
-                    svl_price=svl_price,
-                )
-            else:
-                holder.owner_address = owner_address
-                holder.vin = vin
-                holder.requester_address = requester_address
-                holder.request_accepted = request_accepted
-                holder.current_owner_info = curr_owner_info
-                holder.previous_owners_info = p_o_i
-                holder.svl_price = svl_price
-                await holder.save()
+            holder.owner_address=owner_address
+            holder.vin=vin
+            holder.brand=brand
+            holder.model=model
+            holder.year=year
+            holder.requester_address=requester_address
+            holder.request_accepted=request_accepted
+            holder.current_owner_info=curr_owner_info
+            holder.previous_owners_info=p_o_i
+            holder.svl_price = svl_price
+            await holder.save()
 
 
 
